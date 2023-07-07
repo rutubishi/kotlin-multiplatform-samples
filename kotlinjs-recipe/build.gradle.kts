37721d9904b36016e3cc666403cb2b448d6b68f5
Ian Okumu <okumu.otsembo@gmail.com>
2023-07-07 12:45:54 +0300

    JS Recipe: Add initial files

---
+++ kotlinjs-recipe/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("js") version "1.8.21"
+}
+
+group = "com.otsembo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
+}
+
+kotlin {
+    js {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport {
+                    enabled.set(true)
+                }
+            }
+        }
+    }
+}
