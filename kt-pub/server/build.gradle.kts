f24389d8888a5d3dfda193b4a07385c8dfb8c1a2
Ian Okumu <okumu.otsembo@gmail.com>
2023-09-07 12:14:25 +0300

    [kt-pub] (server) migrate dependencies to version catalog

--- kt-pub/server/build.gradle.kts
+++ kt-pub/server/build.gradle.kts
@@ -10,25 +10,12 @@ kotlin {
         val jvmMain by getting {
             dependencies {
                 implementation(project(":shared"))
-                implementation("io.ktor:ktor-server-core-jvm:2.3.4")
-                implementation("io.ktor:ktor-server-auth-jvm:2.3.4")
-                implementation("io.ktor:ktor-server-auth-jwt-jvm:2.3.4")
-                implementation("io.ktor:ktor-server-host-common-jvm:2.3.4")
-                implementation("io.ktor:ktor-server-status-pages-jvm:2.3.4")
-                implementation("io.ktor:ktor-server-cors-jvm:2.3.4")
-                implementation("io.ktor:ktor-server-swagger-jvm:2.3.4")
-                implementation("io.ktor:ktor-server-openapi:2.3.4")
-                implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.4")
-                implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.4")
-                implementation("io.ktor:ktor-server-netty-jvm:2.3.4")
-                implementation("ch.qos.logback:logback-classic:1.4.11")
-                implementation("io.ktor:ktor-server-config-yaml:2.3.4")
+                implementation(libs.bundles.ktor.server)
              }
         }
         val jvmTest by getting {
             dependencies {
-                implementation("io.ktor:ktor-server-tests-jvm:2.3.4")
-                implementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.0")
+                implementation(libs.bundles.ktor.server.tests)
             }
         }
     }
